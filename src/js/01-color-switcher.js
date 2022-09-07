c7c7834fc9e1f33f1f4734873dcbe165ad6b5ec9
AleksanrKotlyar <kotlyar.alek@gmail.com>
2022-09-07 13:48:19 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+btnStartRef = document.querySelector('[data-start]');
+btnStopRef = document.querySelector('[data-stop]');
 
+btnStartRef.addEventListener('click', onClickBtnStart);
+btnStopRef.addEventListener('click', onClickBtnStop);
+
+let idInt = null;
+
+function onClickBtnStart() {
+  btnStartRef.disabled = true;
+  idInt = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickBtnStop() {
+  btnStartRef.disabled = false;
+  clearInterval(idInt);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
